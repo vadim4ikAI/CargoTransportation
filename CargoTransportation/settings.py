e42eb60ead1ba2d672819926b6cc80365505368c
вадим <vadim@gmail.com>
2025-03-24 16:58:36 +0300

    deploy

--- CargoTransportation/settings.py
+++ CargoTransportation/settings.py
@@ -131,3 +131,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = os.getenv('qwertyuio', "fallback-secret-key")
